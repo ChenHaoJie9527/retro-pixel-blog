7185cf7e26a6a60918cbb6065e3f3c3e36ff2752
momotom <a18520145493@163.com>
2025-03-02 12:28:39 +0800

    feat: 全局报错路由

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import { Press_Start_2P, VT323 } from "next/font/google"
 import "./globals.css";
 
@@ -19,7 +18,7 @@ const vt323 = VT323({
 
 export const metadata: Metadata = {
   title: "Retro Pixel Blog",
-  description: "Retro Pixel Blog",
+  description: "Tech, Art, and Finance tips with a retro twist",
 };
 
 export default function RootLayout({
@@ -30,7 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${pressStart2p.variable} ${vt323.variable} antialiased`}
+        className={`${pressStart2p.variable} ${vt323.variable} font-sans bg-gray-900 text-gray-400 dark:bg-gray-900 dark:text-green-400`}
       >
         {children}
       </body>
