11432c3e99eda7bdb56944d4067ac3f25bc5d58e
momotom <a18520145493@163.com>
2025-03-02 12:17:35 +0800

    feat: 设置网络字体

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,21 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { Press_Start_2P, VT323 } from "next/font/google"
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+const pressStart2p = Press_Start_2P({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-press-start-2p",
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+
+const vt323 = VT323({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-vt323",
 });
+
+
 
 export const metadata: Metadata = {
   title: "Retro Pixel Blog",
@@ -26,7 +30,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${pressStart2p.variable} ${vt323.variable} antialiased`}
       >
         {children}
       </body>
